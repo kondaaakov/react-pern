3b39c3cb5c2cf5caae4c06a2074fb9f8d7aa32a3
Andrew <kondaaakov@gmail.com>
2022-06-24 18:26:59 +0300

    update v.2.2

---
+++ client/src/components/AppRouter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Route, Routes, Navigate } from "react-router-dom";
+
+const AppRouter = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default AppRouter;
