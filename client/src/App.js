1795a8eb2160918fe5bbca42ed57a26312d39f1b
kondaaakov <kondaaakov@gmail.com>
2022-07-01 17:47:30 +0300

    ups

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router} from 'react-router-dom';
+import AppRouter from './components/AppRouter';
 
 function App() {
-  return (
-    <div className="App">
-      WORKING
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                <AppRouter />
+            </Router>
+        </div>
+    );
 }
 
 export default App;
---
+++ client/src/routes/routes.js
@@ -0,0 +1,9 @@
+import Admin from "../pages/Admin"
+
+export const authRoutes = [
+    {path: '/admin', component: <Admin />, exact: true}
+]
+
+export const publicRoutes = [
+
+]
